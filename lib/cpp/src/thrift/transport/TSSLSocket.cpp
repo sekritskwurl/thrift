305de2290f2128f9f51e4d626a3848d17cc37071
sekritskwurl <joseph.hesse@gmail.com>
2017-02-22 17:47:05 +0400

    Update TSSLSocket.cpp

--- lib/cpp/src/thrift/transport/TSSLSocket.cpp
+++ lib/cpp/src/thrift/transport/TSSLSocket.cpp
@@ -154,13 +154,7 @@ static char uppercase(char c);
 
 // SSLContext implementation
 SSLContext::SSLContext(const SSLProtocol& protocol) {
-  if (protocol == SSLTLS) {
-    ctx_ = SSL_CTX_new(SSLv23_method());
-#ifndef OPENSSL_NO_SSL3
-  } else if (protocol == SSLv3) {
-    ctx_ = SSL_CTX_new(SSLv3_method());
-#endif
-  } else if (protocol == TLSv1_0) {
+  if (protocol == TLSv1_0) {
     ctx_ = SSL_CTX_new(TLSv1_method());
   } else if (protocol == TLSv1_1) {
     ctx_ = SSL_CTX_new(TLSv1_1_method());
